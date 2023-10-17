623472542bfd613bea9d4d4ed50d9517c44b4f48
RCast23 <RobertoCastillaSenero@gmail.com>
2023-10-18 00:11:58 +0200

    PreUtiles

--- miguelGonzalez_robertoCastilla_PracticaADFicheros/src/gestores/gestorAlumno.java
+++ miguelGonzalez_robertoCastilla_PracticaADFicheros/src/gestores/gestorAlumno.java
@@ -1,5 +1,25 @@
 package gestores;
 
-public class gestorAlumno {
+import java.util.HashMap;
 
+import clases.*;
+
+public class gestorAlumno {
+	private HashMap<String,Alumno> listaAlumnos;
+	private HashMap<String,Curso> listaCursos;
+	
+	public gestorAlumno(){
+		listaAlumnos = new HashMap<String,Alumno>();
+	}
+	
+	public void matricularAlumno() {
+		System.out.println("Introduzca el nombre del alumno");
+		String nombreAlum;
+		System.out.println("Introduzca los apellidos del alumno");
+		String apellidosAlum;
+		//Alumno alumno = listaAlumnos.get(nombreAlum+apellidosAlum);
+		System.out.println("Introduzca el nombre del curso");
+		String nombreCurso;
+		//Curso curso = listaCursos.get(nombreCurso);
+	}
 }
