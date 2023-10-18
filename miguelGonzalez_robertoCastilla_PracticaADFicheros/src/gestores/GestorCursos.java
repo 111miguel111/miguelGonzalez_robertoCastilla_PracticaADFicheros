485cd51574ecc0dd7d3421c30c1ad980d685b679
111miguel111 <miguelmora111pro@gmail.com>
2023-10-19 00:52:06 +0200

    G profes alums y cursos
    pingas chupa , no enserio antonio lo esta pasando no ironicamente mal

---
+++ miguelGonzalez_robertoCastilla_PracticaADFicheros/src/gestores/GestorCursos.java
@@ -0,0 +1,41 @@
+package gestores;
+
+import clases.*;
+
+public class GestorCursos {
+	
+	public void vinvularProfesor() {
+		
+	}
+	public void desvinvularProfesor() {
+		
+	}
+	public void matricularAlumno() {
+		
+	}
+	public void desmatricularAlumno() {
+		
+	}
+	public void crearCurso() {
+		
+	}
+	public void borrarCurso() {
+		
+	}
+	public void modificarCurso() {
+		
+	}
+	public Curso buscarCurso() {
+		
+		return null;
+	}
+	public void mostrarCurso() {
+		
+	}
+	public void mostrarCursos() {
+		
+	}
+	
+	
+	
+}
---
+++ miguelGonzalez_robertoCastilla_PracticaADFicheros/src/gestores/GestorProfesores.java
@@ -0,0 +1,30 @@
+package gestores;
+import clases.*;
+
+public class GestorProfesores {
+	public void vinvularProfesor() {
+		
+	}
+	public void desvinvularProfesor() {
+		
+	}
+	public void crearProfesor() {
+		
+	}
+	public void borrarProfesor() {
+		
+	}
+	public void modificarProfesor() {
+		
+	}
+	public Profesor buscarProfesor() {
+		return null;
+	}
+	public void mostrarProfesor() {
+		
+	}
+	public void mostrarProfesores() {
+		
+	}
+	
+}
