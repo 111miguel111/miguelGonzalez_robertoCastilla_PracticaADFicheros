1fb88886890a894c7e8942714c026c030ea51a16
RCast23 <RobertoCastillaSenero@gmail.com>
2023-11-14 18:30:28 +0100

    Fin comentarios GestorDatos

--- miguelGonzalez_robertoCastilla_PracticaADFicheros/src/gestores/GestorDatos.java
+++ miguelGonzalez_robertoCastilla_PracticaADFicheros/src/gestores/GestorDatos.java
@@ -45,6 +45,7 @@ public class GestorDatos {
 
 	/**
 	 * Metodo para escribir un alumno
+	 * 
 	 * @param alumno Alumno a escribir
 	 */
 	public static void escribirAlum(Alumno alumno) {
@@ -57,6 +58,7 @@ public class GestorDatos {
 
 	/**
 	 * Metodo para escribir todos los alumnos
+	 * 
 	 * @param listaAlum HashMap con todos los alumnos
 	 */
 	public static void escribirTodosAlum(HashMap<String, Alumno> listaAlum) {
@@ -64,7 +66,7 @@ public class GestorDatos {
 		try {
 			FileOutputStream fileOut = new FileOutputStream(archivoAlum);
 			ObjectOutputStream salida = new ObjectOutputStream(fileOut);
-			
+
 			// Se escribe el contador de alumnos
 			salida.writeInt(Alumno.getCont());
 			// Se recorre la lista de alumnos y se escriben todos
@@ -74,7 +76,7 @@ public class GestorDatos {
 			// Se cierran el fileOutput y el object output
 			salida.close();
 			fileOut.close();
-			
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			System.out.println("Error al escribir el archivo");
@@ -83,7 +85,8 @@ public class GestorDatos {
 
 	/**
 	 * Metodo que busca un alumno
-	 * @param nombre String del nombre del alumno
+	 * 
+	 * @param nombre    String del nombre del alumno
 	 * @param apellidos String de los apellidos del alumno
 	 * @return Alumno deseado o null si no lo encuentra
 	 */
@@ -98,7 +101,7 @@ public class GestorDatos {
 			// Se define el object input stream
 			entrada = new ObjectInputStream(new FileInputStream(archivoAlum));
 			try {
-
+				//Se salta la linea del contador de alumnos del archivo
 				entrada.readInt();
 				// Se busca en el archivo el alumno hasta que lo encuentre o hasta que no queden
 				// mas
@@ -140,16 +143,22 @@ public class GestorDatos {
 
 	/**
 	 * Metodo que devuelve una lista de alumnos
+	 * 
 	 * @return Hashmap con todos los alumnos
 	 */
 	public static HashMap<String, Alumno> getListaAlum() {
 		HashMap<String, Alumno> listaAlum = new HashMap<String, Alumno>();
+		// Se comprueba si el archivo existe
 		alumFileCheck();
+		// Se crea el object input stream y se abre un try para trabajar con el
 		ObjectInputStream entrada = null;
 		try {
+			// Se define el object input stream
 			entrada = new ObjectInputStream(new FileInputStream("alumnos.ser"));
 			try {
+				//Se salta la linea del contador de alumnos del archivo
 				entrada.readInt();
+				//Se leen todos los alumnos y se meten en un hashmap
 				while (true) {
 					Alumno alumno = (Alumno) entrada.readObject();
 					listaAlum.put(alumno.getNombre() + "_" + alumno.getApellidos(), alumno);
@@ -184,7 +193,8 @@ public class GestorDatos {
 	// Metodos profesores ------------------------------------------------
 
 	/**
-	 * Metodo que comprueba si existe el archivo de profesores y si no existe lo crea
+	 * Metodo que comprueba si existe el archivo de profesores y si no existe lo
+	 * crea
 	 */
 	public static void profFileCheck() {
 		if (!archivoProf.exists()) {
@@ -199,6 +209,7 @@ public class GestorDatos {
 
 	/**
 	 * Metodo para escribir todos los profesores
+	 * 
 	 * @param listaProf HashMap con todos los profesores
 	 */
 	public static void escribirTodosProf(HashMap<String, Profesor> listaProf) {
@@ -221,6 +232,7 @@ public class GestorDatos {
 
 	/**
 	 * Metodo para escribir un profesor
+	 * 
 	 * @param profesor Profesor a escribir
 	 */
 	public static void escribirProf(Profesor profesor) {
@@ -233,6 +245,7 @@ public class GestorDatos {
 
 	/**
 	 * Metodo que busca un profesor
+	 * 
 	 * @param dni DNI del profesor a buscar
 	 * @return Profesor deseado o null si no lo encuentra
 	 */
@@ -286,7 +299,7 @@ public class GestorDatos {
 		return profesor;
 	}
 
-	//Metodo que busca todos los profesores
+	// Metodo que busca todos los profesores
 	public static HashMap<String, Profesor> getListaProf() {
 		HashMap<String, Profesor> listaProf = new HashMap<String, Profesor>();
 		profFileCheck();
@@ -294,6 +307,7 @@ public class GestorDatos {
 		try {
 			entrada = new ObjectInputStream(new FileInputStream(archivoProf));
 			try {
+				// Se leen profesores y se meten en la lista
 				while (true) {
 					Profesor profesor = (Profesor) entrada.readObject();
 					listaProf.put(profesor.getDni(), profesor);
@@ -343,6 +357,7 @@ public class GestorDatos {
 
 	/**
 	 * Metodo para escribir todos los cursos
+	 * 
 	 * @param listaCursos HashMap con todos los cursos
 	 */
 	public static void escribirTodosCursos(HashMap<String, Curso> listaCursos) {
@@ -352,7 +367,7 @@ public class GestorDatos {
 			BufferedWriter bw = new BufferedWriter(myWriter);
 
 			// Se escribe el contador estatico de cursos
-			bw.write(Curso.getCont()+"\n");
+			bw.write(Curso.getCont() + "\n");
 
 			// Se recorre la lista de cursos escribiendo cada valor de forma individual
 			for (Map.Entry<String, Curso> i : listaCursos.entrySet()) {
@@ -370,6 +385,7 @@ public class GestorDatos {
 
 	/**
 	 * Metodo para escribir un curso
+	 * 
 	 * @param curso Curso a escribir
 	 */
 	public static void escribirCurso(Curso curso) {
@@ -382,6 +398,7 @@ public class GestorDatos {
 
 	/**
 	 * Metodo que busca un curso
+	 * 
 	 * @param nombre String del nombre del curso
 	 * @return Curso deseado o null si no lo encuentra
 	 */
@@ -389,23 +406,29 @@ public class GestorDatos {
 		Curso curso = null;
 		try {
 			Scanner sc = new Scanner(archivoCurso);
-			//Saltar contador
+			// Saltar contador
 			if (sc.hasNextLine()) {
 				sc.nextLine();
 			}
+			// Se va leyendo el archivo de 3 en 3, ya que el contenido de cada curso se ha escrito en 3 lineas
 			while (sc.hasNextLine()) {
 				String cursoTexto = sc.nextLine();
 				String cursoAlum = sc.nextLine();
 				String cursoProf = sc.nextLine();
+				// Se comprueba el nombre del curso es el nombre que se busca
 				if (cursoTexto.split("¬")[1].equals(nombre)) {
 					curso = new Curso(cursoTexto.split("¬")[1], cursoTexto.split("¬")[2]);
 					curso.setCodigo(cursoTexto.split("¬")[0]);
+					// Se comprueba que el texto de profesor no sea un campo vacio y se crea
 					if (cursoProf != "") {
 						curso.setProfesor(new Profesor(cursoProf.split("¬")[0], cursoProf.split("¬")[1],
 								cursoProf.split("¬")[2], cursoProf.split("¬")[3]));
 					}
+					// Se crea el hashmap y se comprueba que haya alumnos, si los hay se introducen
+					// 1 a 1
 					HashMap<String, Alumno> listaAlum = new HashMap<String, Alumno>();
 					if (cursoAlum != "") {
+						// En las lineas de texto los alumnos ocupan 5 campos asi que se suman de 5 en 5
 						for (int i = 0; i < cursoAlum.split("¬").length; i++) {
 							listaAlum.put(cursoAlum.split("¬")[i] + "_" + cursoAlum.split("¬")[i + 1],
 									new Alumno(cursoAlum.split("¬")[i], cursoAlum.split("¬")[i + 1],
@@ -414,6 +437,7 @@ public class GestorDatos {
 							i += 5;
 						}
 					}
+					// Se guardan los alumnos en el curso
 					curso.setAlumnos(listaAlum);
 					sc.close();
 					return curso;
@@ -427,33 +451,38 @@ public class GestorDatos {
 		return curso;
 
 	}
-	
+
 	/**
 	 * Metodo que devuelve un HashMap con todos los cursos
+	 * 
 	 * @return HashMap con todos los cursos
 	 */
 	public static HashMap<String, Curso> getListaCursos() {
 		HashMap<String, Curso> listaCurso = new HashMap<String, Curso>();
 		try {
 			Scanner sc = new Scanner(archivoCurso);
-			//Saltar contador
+			// Saltar contador
 			if (sc.hasNextLine()) {
 				sc.nextLine();
 			}
-			// Aqui estaba sumando contador
+			// Se va leyendo el archivo de 3 en 3, ya que el contenido de cada curso se ha escrito en 3 lineas
 			while (sc.hasNextLine()) {
 				String cursoTexto = sc.nextLine();
 				String cursoAlum = sc.nextLine();
 				String cursoProf = sc.nextLine();
+				//Se comprueba si curso esta vacio y si no lo esta se crea el curso
 				if (cursoTexto != "") {
 					Curso curso = new Curso(cursoTexto.split("¬")[1], cursoTexto.split("¬")[2]);
 					curso.setCodigo(cursoTexto.split("¬")[0]);
+					//Se comprueba si la linea del profesor esta vacio y si no lo crea y agrega al curso
 					if (cursoProf != "") {
 						curso.setProfesor(new Profesor(cursoProf.split("¬")[0], cursoProf.split("¬")[1],
 								cursoProf.split("¬")[2], cursoProf.split("¬")[3]));
 					}
 					HashMap<String, Alumno> listaAlum = new HashMap<String, Alumno>();
+					//Se comprueba si la linea de alumno esta vacio y si no lo crea y agrega todos los alumnos a un hasmap, que posteriormente se agregaga al curso
 					if (cursoAlum != "") {
+						// En las lineas de texto los alumnos ocupan 5 campos asi que se suman de 5 en 5
 						for (int i = 0; i < cursoAlum.split("¬").length; i++) {
 							listaAlum.put(cursoAlum.split("¬")[i] + "_" + cursoAlum.split("¬")[i + 1],
 									new Alumno(cursoAlum.split("¬")[i], cursoAlum.split("¬")[i + 1],
@@ -462,7 +491,6 @@ public class GestorDatos {
 							i += 5;
 						}
 					}
-
 					curso.setAlumnos(listaAlum);
 					listaCurso.put(curso.getNombre(), curso);
 				}
@@ -477,16 +505,19 @@ public class GestorDatos {
 
 	// Otros-------------------------------------------
 	/**
-	 * Metodo para cargar los contadores de Alumno y Curso para numero de expediente y codigo respectivamente
+	 * Metodo para cargar los contadores de Alumno y Curso para numero de expediente
+	 * y codigo respectivamente
 	 */
 	public static void setContadores() {
+		//Se comprueba que existatn los archivos de alumno y curso
 		alumFileCheck();
 		cursoFileCheck();
 		ObjectInputStream entrada = null;
 		Scanner sc = null;
 		try {
+			//Se lee el primer y unico int del fichero de alumno que es el contador y se guarda
 			entrada = new ObjectInputStream(new FileInputStream(archivoAlum));
-			Alumno.setCont((int) entrada.readInt());			
+			Alumno.setCont((int) entrada.readInt());
 		} catch (IOException e) {
 			System.out.println("No se han podido cargar los contadores");
 		} finally {
@@ -501,12 +532,13 @@ public class GestorDatos {
 			}
 		}
 		try {
+			//Se lee la primera linea del archivo que es el contador y se comprueba que sea digito antes de guardarlo
 			sc = new Scanner(archivoCurso);
 			String contador = sc.nextLine();
 			if (Utiles.esDigito(contador)) {
 				Curso.setCont(Integer.parseInt(contador));
 			}
-			
+
 		} catch (IOException e) {
 			System.out.println("No se han podido cargar los contadores");
 		} catch (NoSuchElementException e) {
