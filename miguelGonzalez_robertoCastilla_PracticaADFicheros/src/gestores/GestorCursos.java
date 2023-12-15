23337ed3a06e0d40f69785224843c18562ed55a4
111miguel111 <miguelmora111pro@gmail.com>
2023-12-15 15:30:18 +0100

    adasd

--- miguelGonzalez_robertoCastilla_PracticaADFicheros/src/gestores/GestorCursos.java
+++ miguelGonzalez_robertoCastilla_PracticaADFicheros/src/gestores/GestorCursos.java
@@ -322,9 +322,27 @@ public class GestorCursos {
 					if (Utiles.confirmarAccion() == null) {
 						
 						HashMap<String, Profesor> Profesores = GestorDatos.getListaProf();
+						for(HashMap.Entry<String, Profesor> entryAux : Profesores.entrySet()) {
+							Profesor profesorAux=entryAux.getValue();
+							HashMap<String, Curso> cursosProfesor=profesorAux.getCursos();
+							for(HashMap.Entry<String, Curso> entryAuxCursoProfesor :cursosProfesor.entrySet()) {
+								if(entryAuxCursoProfesor.getValue()==cursos.get(curso.getNombre())) {
+									entryAuxCursoProfesor.getValue().setNombre(curso3.getNombre());
+									entryAuxCursoProfesor.getValue().setDescripcion(curso3.getDescripcion());
+								}
+							}
+						}
 						HashMap<String, Alumno> Alumnos = GestorDatos.getListaAlum();
-						
-						
+						for(HashMap.Entry<String, Alumno> entryAux : Alumnos.entrySet()) {
+							Alumno alumnoAux=entryAux.getValue();
+							HashMap<String, Curso> cursosAlumno=alumnoAux.getCursos();
+							for(HashMap.Entry<String, Curso> entryAuxCursoAlumno :cursosAlumno.entrySet()) {
+								if(entryAuxCursoAlumno.getValue()==cursos.get(curso.getNombre())) {
+									entryAuxCursoAlumno.getValue().setNombre(curso3.getNombre());
+									entryAuxCursoAlumno.getValue().setDescripcion(curso3.getDescripcion());
+								}
+							}
+						}
 						cursos.get(curso.getNombre()).setNombre(curso3.getNombre());
 						cursos.get(curso.getNombre()).setDescripcion(curso3.getDescripcion());
 						GestorDatos.escribirTodosCursos(cursos);
